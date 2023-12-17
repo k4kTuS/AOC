54a4a965dee41ad9cb347607cf88249cfb297b19
matus <matus.botek@gmail.com>
2023-12-17 14:32:13 +0100

    Day17

---
+++ Day17/day17.py
@@ -0,0 +1,50 @@
+import heapq
+
+def find_path(grid, ultra=False):
+    neighbours = [(0,1), (1,0), (0,-1), (-1,0)]
+    states = set()
+    q = [(0,0,0,0,0,0)]
+    while q:
+        cost, pos_y, pos_x, m_y, m_x, steps = heapq.heappop(q)
+        if pos_y == len(grid)-1 and pos_x == len(grid[-1])-1:
+            return(cost)
+        if (pos_y, pos_x, m_y, m_x, steps) in states:
+            continue
+        states.add((pos_y, pos_x, m_y, m_x, steps))
+
+        m = (m_y, m_x)
+        rev_turn = (-m_y, -m_x)
+
+        if not ultra:
+            for n in neighbours:
+                if n == rev_turn or (n == m and steps == 3):
+                    continue
+
+                n_y = pos_y + n[0]
+                n_x = pos_x + n[1]
+                n_steps = 1 if n != m else steps + 1
+
+                if n_y < 0 or n_x < 0 or n_y == len(grid) or n_x == len(grid[n_y]):
+                    continue
+                heapq.heappush(q, (cost+grid[n_y][n_x],n_y, n_x,n[0],n[1],n_steps))
+        else:
+            for n in neighbours:
+                if n == rev_turn or (n == m and steps == 10) or (n != m and steps < 4 and m != (0,0)):
+                    continue
+                
+                n_y = pos_y + n[0]
+                n_x = pos_x + n[1]
+                n_steps = 1 if n != m else steps + 1
+
+                if n_y < 0 or n_x < 0 or n_y == len(grid) or n_x == len(grid[n_y]):
+                    continue
+                heapq.heappush(q, (cost+grid[n_y][n_x],n_y, n_x,n[0],n[1],n_steps))
+
+with open("Day17/input.txt") as file:
+    lines = [x.rstrip() for x in file.readlines()]
+    grid = [list(map(int, line)) for line in lines]
+    
+    print(find_path(grid, False)) #p1
+    print(find_path(grid, True)) #p2
+
+
